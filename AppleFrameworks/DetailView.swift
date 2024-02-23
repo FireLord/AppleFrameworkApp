9bf73eb2b5e0272244618ec67079de2160b0f4a8
FireLord <firelord.xda@gmail.com>
2024-02-23 15:05:02 +0530

    viewModel: Setup viewmodel to pass data around with sheet

--- AppleFrameworks/DetailView.swift
+++ AppleFrameworks/DetailView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct DetailView: View {
     var framework: Framework
+    @Binding var isShowingDetailView: Bool
     
     var body: some View {
         VStack {
             HStack {
                 Spacer()
                 Button {
-                    
+                    isShowingDetailView = false
                 } label: {
                     Image(systemName: "xmark")
                         .foregroundColor(Color(.label))
@@ -46,5 +47,5 @@ struct DetailView: View {
 }
 
 #Preview {
-    DetailView(framework: MockData.sampleFramework)
+    DetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
 }
---
+++ AppleFrameworks/viewModel/FrameworkGridViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridViewModel.swift
+//  AppleFrameworks
+//
+//  Created by Aman Kumar on 23/02/24.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    var selectedFramework: Framework? {
+        // whenever the framework is selected the value will change and show the detailView page
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    @Published var isShowingDetailView = false
+}
